fa0bcf37fb274306659165dd9a0d91a45b9a2094
Robert Coe <chakrawc@gmail.com>
2022-09-12 18:52:31 -0700

    fix/namespace-api (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from "./api";
+import * as api from "./api";
 export * from "./contexts";
 export * from "./Game";
 export * from "./helpers";
 export * from "./PlayerClass";
+export { api };
